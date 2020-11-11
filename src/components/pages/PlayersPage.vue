d9d0cdee181cd73bfdd156eb6c31a780a57846d0
diego <masnimpu@gmail.com>
2020-11-11 23:36:05 +0300

    Activity module

--- src/components/pages/PlayersPage.vue
+++ src/components/pages/PlayersPage.vue
@@ -1,40 +1,40 @@
 <template>
-  <div>
-    <b-container>
-      <players-table-filter :handler="handleFilterChanges" class="mb-3"></players-table-filter>
-      <div class="my-3">
-        Отображено результатов: <b>{{ items.length }}</b>
-      </div>
-      <b-table :busy="busy" bordered :items="items" :fields="fields">
-        <template v-slot:cell(name)="row">
-          <player-name :player="row.item"></player-name>
-        </template>
-        <template v-slot:cell(rank.id)="row">
-          {{ row.item.rank.title }}
-        </template>
-        <template v-slot:cell(class)="row">
-          {{ row.item.class.title }}
-        </template>
-        <template v-slot:cell(spec)="row">
-          <img class="spec-image" :src="row.item.role.image"/>
-          {{ row.item.specialization.title }}
-        </template>
-        <template v-slot:cell(gear)="row">
-          {{ row.item.gear }}
-        </template>
-        <template v-slot:cell(activity)="row">
-          <b-progress max="1" :value="playerActivity(row.item)"/>
-        </template>
-        <template v-slot:cell(realname)="row">
-          {{ row.item.raw.realname }}
-        </template>
-      </b-table>
-      <margin top="20" bottom="20" class="text-muted">
-        Мета информация:
-      </margin>
-      <b-textarea readonly v-model="metaText" rows="6"/>
-    </b-container>
-  </div>
+    <div>
+        <b-container>
+            <players-table-filter :handler="handleFilterChanges" class="mb-3"></players-table-filter>
+            <div class="my-3">
+                Отображено результатов: <b>{{ items.length }}</b>
+            </div>
+            <b-table :busy="busy" bordered :items="items" :fields="fields">
+                <template v-slot:cell(name)="row">
+                    <player-name :player="row.item"></player-name>
+                </template>
+                <template v-slot:cell(rank.id)="row">
+                    {{ row.item.rank.title }}
+                </template>
+                <template v-slot:cell(class)="row">
+                    {{ row.item.class.title }}
+                </template>
+                <template v-slot:cell(spec)="row">
+                    <img class="spec-image" :src="row.item.role.image"/>
+                    {{ row.item.specialization.title }}
+                </template>
+                <template v-slot:cell(gear)="row">
+                    {{ row.item.gear }}
+                </template>
+                <template v-slot:cell(activity)="row">
+                    <player-activity :player="row.item"/>
+                </template>
+                <template v-slot:cell(realname)="row">
+                    {{ row.item.raw.realname }}
+                </template>
+            </b-table>
+            <margin top="20" bottom="20" class="text-muted">
+                Мета информация:
+            </margin>
+            <b-textarea readonly v-model="metaText" rows="6"/>
+        </b-container>
+    </div>
 </template>
 
 <script>
@@ -47,92 +47,87 @@ import Guild from "@/app/Guild";
 import PlayerName from "../player/PlayerName";
 import PlayersButch from "@/app/butches/PlayersButch";
 import Margin from "@/components/utils/Margin";
+import PlayerActivity from "@/modules/Player/components/PlayerActivity";
 
 export default {
-  name: "PlayersPage",
-  components: {PlayerName, PlayersTableFilter, Margin},
-  data() {
-    return {
-      busy: false,
-      metaText: '...',
-      items: [],
-      filter: {
-        specs: [
-          PlayersButch.Filter.TANKS, PlayersButch.Filter.HEALERS,
-          PlayersButch.Filter.RANGED, PlayersButch.Filter.MILLIE
-        ],
-        etc: [
-          PlayersButch.Filter.IN_GUILD, PlayersButch.Filter.MAX_LEVEL_ONLY
-        ],
-        name: "",
-      },
-      images: {
-        shield,
-        bow,
-        heal,
-        swords
-      },
-      fields: [
-        {key: "level", label: "Уровень", sortable: true},
-        {key: "name", label: "Имя"},
-        {key: "rank.id", label: "Ранг", sortable: true},
-        {key: "class", label: "Класс"},
-        {key: "activity", label: "Активность", sortable: true},
-        {key: "realname", label: "Заметка", sortable: true}
-      ]
-    }
-  },
-  mounted() {
-    Guild.shared.wait(this.applyTable.bind(this));
-  },
-  methods: {
-    applyTable() {
-      this.busy = true;
-      this.items = Guild.shared.createPlayersButch()
-          .filter(
-              ...this.filter.specs,
-              ...this.filter.etc
-          )
-          .sort(
-              PlayersButch.Sorting.FIRST_HIGH_RANK,
-              PlayersButch.Sorting.FIRST_HIGH_LEVEL,
-              PlayersButch.Sorting.ALPHABETIC
-          )
-          .getButchWithName(this.filter.name);
-      this.countMetaData();
-      this.busy = false;
+    name: "PlayersPage",
+    components: {PlayerName, PlayersTableFilter, Margin, PlayerActivity},
+    data() {
+        return {
+            busy: false,
+            metaText: '...',
+            items: [],
+            filter: {
+                specs: [
+                    PlayersButch.Filter.TANKS, PlayersButch.Filter.HEALERS,
+                    PlayersButch.Filter.RANGED, PlayersButch.Filter.MILLIE
+                ],
+                etc: [
+                    PlayersButch.Filter.IN_GUILD, PlayersButch.Filter.MAX_LEVEL_ONLY
+                ],
+                name: "",
+            },
+            images: {
+                shield,
+                bow,
+                heal,
+                swords
+            },
+            fields: [
+                {key: "level", label: "Уровень", sortable: true},
+                {key: "name", label: "Имя"},
+                {key: "rank.id", label: "Ранг", sortable: true},
+                {key: "class", label: "Класс"},
+                {key: "activity", label: "Активность", sortable: true},
+                {key: "realname", label: "Заметка", sortable: true}
+            ]
+        }
     },
+    mounted() {
+        Guild.shared.wait(this.applyTable.bind(this));
+    },
+    methods: {
+        applyTable() {
+            this.busy = true;
+            this.items = Guild.shared.createPlayersButch()
+                .filter(
+                    ...this.filter.specs,
+                    ...this.filter.etc
+                )
+                .sort(
+                    PlayersButch.Sorting.FIRST_HIGH_RANK,
+                    PlayersButch.Sorting.FIRST_HIGH_LEVEL,
+                    PlayersButch.Sorting.ALPHABETIC
+                )
+                .getButchWithName(this.filter.name);
+            this.countMetaData();
+            this.busy = false;
+        },
 
-    playerActivity(player) {
-      const total = (player.guildScore.lastWeek || 0) +
-          (player.guildScore.thisWeek || 0);
-      return Math.round(total / Guild.shared.maxActivity);
-    },
+        countMetaData() {
+            this.metaText = '';
+            this.items.forEach(player => {
+                this.metaText += player.name + '-Гордунни|';
+                this.metaText += player.role.title + '-' + Math.floor(player.gear) + " ";
+                if (player.raw.realname) this.metaText += `(${player.raw.realname})`;
+                this.metaText += "\n";
+            });
+        },
 
-    countMetaData() {
-      this.metaText = '';
-      this.items.forEach(player => {
-        this.metaText += player.name + '-Гордунни|';
-        this.metaText += player.role.title + '-' + Math.floor(player.gear) + " ";
-        if (player.raw.realname) this.metaText += `(${player.raw.realname})`;
-        this.metaText += "\n";
-      });
+        handleFilterChanges(specs, etc, name) {
+            this.filter.specs = specs;
+            this.filter.etc = etc;
+            this.filter.name = name;
+            this.applyTable();
+        },
     },
-
-    handleFilterChanges(specs, etc, name) {
-      this.filter.specs = specs;
-      this.filter.etc = etc;
-      this.filter.name = name;
-      this.applyTable();
-    },
-  },
 }
 </script>
 
 <style>
 textarea {
-  background-color: #2a2a2a !important;
-  border-color: #16181b !important;
-  color: #b5b5b5 !important;
+    background-color: #2a2a2a !important;
+    border-color: #16181b !important;
+    color: #b5b5b5 !important;
 }
 </style>
---
+++ src/modules/Player/components/PlayerActivity.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <b-progress
+            v-b-tooltip.hover :title="`Активность ${proc}`"
+            max="1"
+            :value="playerActivity()"
+        />
+    </div>
+</template>
+
+<script>
+import Guild from "@/app/Guild";
+
+export default {
+    name: "PlayerActivity",
+    props:{
+      player: null,
+    },
+    data: () => ({
+        proc: "0%"
+    }),
+    methods: {
+        playerActivity() {
+            const total = (this.player.guildScore.lastWeek || 0) +
+                (this.player.guildScore.thisWeek || 0);
+            const dif = total / Guild.shared.maxActivity;
+            this.proc = (Math.round(dif * 10000) / 100) + "%";
+            return Math.round(dif);
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
