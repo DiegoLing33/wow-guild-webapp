158ad7420b3894bd36e6d55ec219a0213b53f962
diego <masnimpu@gmail.com>
2020-11-23 03:10:25 +0300

    hot fix server authorization

--- src/modules/Login/pages/CallbackPage.vue
+++ src/modules/Login/pages/CallbackPage.vue
@@ -5,11 +5,17 @@
 </template>
 
 <script>
+import {GuildAPI} from "prestij.xyz-api";
+
 export default {
     name: "CallbackPage",
     mounted() {
 
         this.$store.dispatch("initLogin").then(() => {
+            GuildAPI.AddUser({
+                blizzard_name: this.$store.getters.battleTag,
+                token: this.$store.getters.authToken,
+            });
             this.$router.push('/');
         });
 
